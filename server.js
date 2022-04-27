aa2d17c1e9a963c9a642e0aa1412b6f10c6d53a4
PatriotxZero <Coreylong90@icloud.com>
2022-04-27 09:21:56 -0400

    background and logo for header added to public directory

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const router = require('./controllers/router')
+require('ejs')
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -8,10 +9,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(router)
 
-//KEEP FOR QUICK TROUBLESHOOTING
-// app.get('/', (req,res) => {
-//     res.send('it works!')
-// })
 app.listen(4444, () => {
     console.log('listening on port 4444')
 })
