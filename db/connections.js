47a0cb9541b8234f2a71c85f93f00a7c028e3548
PatriotxZero <Coreylong90@icloud.com>
2022-04-27 08:15:47 -0400

    used dotenv to hide private mongoDB login and collections location

--- db/connections.js
+++ db/connections.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-
 const mongoURI =
 process.env.NODE_ENV === 'production'
     ? process.env.DB_URL
-    : 'mongodb+srv://admin:QAZxsw90@cluster0.dal9c.mongodb.net/outpost?retryWrites=true&w=majority'
+    : process.env.DEV_DB_URL
 
-mongoose.connect(mongoURI)
+mongoose
+.connect(mongoURI)
 .then((instance) => 
         console.log(`connected to db: ${instance.connections[0].name}`))
         .catch((error) => console.log('connection error', error))
