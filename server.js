a679a47d270c6abfd01a71b125193bb346f38168
PatriotxZero <Coreylong90@icloud.com>
2022-04-27 10:04:22 -0400

    sample seed written in JSON file. partials set up however getting a cannot access views error. troubleshooting now

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 require('dotenv').config()
 const express = require('express')
+const app = express()
+const ejsLayouts = require('express-ejs-layouts')
 const router = require('./controllers/router')
 require('ejs')
 
-const app = express()
+
 app.set('view engine', 'ejs')
+app.use(ejsLayouts)
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(router)
+
 
 app.listen(4444, () => {
     console.log('listening on port 4444')
