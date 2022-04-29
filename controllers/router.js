30b738c52efe53b6daf5c7c0fadc1791bc68b407
PatriotxZero <Coreylong90@icloud.com>
2022-04-29 14:06:53 -0400

    full CRUD abilities have been fixed and verified

--- controllers/router.js
+++ controllers/router.js
@@ -10,8 +10,40 @@ router.get('/', (req,res) => {
 
 router.post('/newPost', (req,res) => {
     Blog.create(req.body)
-    .then((blog) => res.redirect("/"))
+    .then(() => res.redirect("/"))
     console.log('adventure added')
+})
+
+router.get('/:id/edit', (req,res) => {
+    const id = req.params.id;
+    console.log(id)
+    Blog.findById(id)
+    .then((blog) => {
+        res.render('edit', { blog: blog })
+    })
+    .catch(console.error)
+})
+
+router.put('/:id', (req,res) => {
+    const id = req.params.id
+    Blog.findOneAndUpdate(
+        { _id: id },
+        {
+            title: req.body.title,
+            location: req.body.location,
+            activity: req.body.activity,
+            content: req.body.content,
+        },
+        { new: true}
+    )
+    .then((blog) => {
+        res.redirect('/')
+    })
+    .catch(console.error)
+})
+
+router.delete('/:id', (req,res) => {
+    Blog.findOneAndDelete({ _id: req.params.id }).then(() => res.redirect('/'))
 })
 
 
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
-// const ejsLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override')
+// const bodyParser = require('body-parser')
 const router = require('./controllers/router')
 require('ejs')
 
-// app.use(ejsLayouts)
+///DB & APP CONFIG
+const app = express()
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+// app.use(bodyParser.urlencoded({extended: true}))
 app.use(router)
 app.set('view engine', 'ejs')
 
